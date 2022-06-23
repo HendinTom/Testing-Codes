c36a9d3c6658a3973af1d734f50cc7d862ecf95f
HendinTom <HeninTomV.H@gmail.com>
2022-06-23 19:17:50 -0400

    knitters

--- github.py
+++
@@ -1,1 +0,0 @@
-print("Testing out Github with Visual Studio Code on a Chromebook!!!");
---
+++ index.js
@@ -0,0 +1,8 @@
+function monkey() {
+    document.getElementById('p1').innerHTML = "He's a knitter!";
+    document.getElementById('p2').innerHTML = "KNITTER!";
+    document.getElementById('p3').innerHTML = "KKKNNIITTTERRRR!";
+    document.getElementById('p4').innerHTML = "NOOOOOOOOOOO!!!";
+    document.getElementById('p5').innerHTML = "NO! NO! I'm not a knitter";
+    document.getElementById('video').style.display = "block";
+}
